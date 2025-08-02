8e1a122f2465aa60f265d625047d70554140f2eb
kulkarnishrinivas <shrinivaskulkarni989@gmail.com>
2025-08-02 17:01:29 +0530

    updated analyticsController with overall anlytic for user

--- src/controllers/analyticsController.js
+++ src/controllers/analyticsController.js
@@ -101,3 +101,42 @@ exports.getTopicAnalytics = async(req, res) =>{
     }
 };
 
+
+
+
+exports.getOverallAnalytics = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const urls = await URL.find({ user: userId });
+    const urlIds = urls.map(url => url._id);
+
+    const totalUrls = urls.length;
+    const totalClicks = await Click.countDocuments({ url: { $in: urlIds } });
+    const uniqueClicks = (await Click.distinct('ipAddress', { url: { $in: urlIds } })).length;
+
+    const clicksByDate = await Click.aggregate([
+        { $match: { url: { $in: urlIds }, timestamp: { $gte: moment().subtract(7, 'days').toDate() } } },
+        { $group: { _id: { $dateToString: { format: '%Y-%m-%d', date: '$timestamp' } }, totalClicks: { $sum: 1 } } },
+        { $sort: { _id: 1 } },
+        { $project: { _id: 0, date: '$_id', totalClicks: '$totalClicks' } },
+    ]);
+
+    const osType = await Click.aggregate([
+        { $match: { url: { $in: urlIds } } },
+        { $group: { _id: '$osType', uniqueClicks: { $addToSet: '$ipAddress' } } },
+        { $project: { _id: 0, osName: '$_id', uniqueClicks: { $size: '$uniqueClicks' } } },
+    ]);
+
+    const deviceType = await Click.aggregate([
+        { $match: { url: { $in: urlIds } } },
+        { $group: { _id: '$deviceType', uniqueClicks: { $addToSet: '$ipAddress' } } },
+        { $project: { _id: 0, deviceName: '$_id', uniqueClicks: { $size: '$uniqueClicks' } } },
+    ]);
+
+    res.status(200).json({ totalUrls, totalClicks, uniqueClicks, clicksByDate, osType, deviceType });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
