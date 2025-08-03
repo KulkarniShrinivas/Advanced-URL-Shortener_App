0d4c518298d5c6e06ac6baa7093c62c7ec0e2dab
Shrinivas Kulkarni <shrinivassk.mca23@rvce.edu.in>
2025-08-03 18:59:31 +0530

    updated redirect routes

--- src/routes/redirectRoutes.js
+++ src/routes/redirectRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { redirectToLongUrl } = require('../controllers/urlController');
 
+router.get('/', (req, res) => {
+    res.redirect('/api/auth/google');
+});
+
 router.get('/:alias', redirectToLongUrl);
 
 module.exports = router;
