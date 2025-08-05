d0514d6a25872b92d914491f6d76851f9a52d47c
Shrinivas Kulkarni <shrinivassk.mca23@rvce.edu.in>
2025-08-05 16:49:00 +0530

    updated

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -4,7 +4,7 @@ const createUrlLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
   max: 10,
   message: 'You have exceeded the URL creation limit. Please try again later.',
-  keyGenerator: (req) => req.user.id,
+  keyGenerator: (req) => req.ip,
 });
 
 module.exports = { createUrlLimiter };
